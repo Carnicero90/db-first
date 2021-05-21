995deba1bff5e0911e4b05e105f2afd9465c9811
Filippo Montani <78483736+Carnicero90@users.noreply.github.com>
2021-05-21 12:49:13 +0200

    aggiunti commenti e nuovo field (fuel_category)

--- cars.sql
+++ cars.sql
@@ -4,16 +4,17 @@ CREATE TABLE cars (
 	brand VARCHAR(10) NOT NULL,
 	price FLOAT(10,2) NOT NULL,
 	car_year YEAR,
-	status_description TEXT(500),
+	status_description TEXT(500), /* descrizione auto + problemi eventuali */
 	mileage_in_km INT(10) NOT NULL,
-	euro_rank INT(1),
+	euro_rank INT(1), /* vorrebbe essere per le classi ambientali: euro1, euro2, etc */
 	plate VARCHAR(10) NOT NULL UNIQUE,
 	pics VARCHAR(10),
 	ad_date DATE,
-	PRIMARY KEY ( car_id )
+	PRIMARY KEY ( car_id ),
+	fuel_category TINYINT DEFAULT 0 /* 'finto' richiamo ad altra table, con tipi carburante: 0=benzina, 1=diesel, etc */
 );
+
 INSERT INTO cars (model, price, car_year, brand, mileage_in_km, plate) VALUES ('panda', 500, 2007, 'fiat', 300, 'abc123');
 INSERT INTO cars (model, price, car_year, brand, mileage_in_km, plate) VALUES ('bohboh', 100000, 2018, 'tesla', 4000, 'abd123'); 
+
 SELECT * FROM cars;
-
-
